7c5ebea8abaf328862997b298f35d626d475bfb3
geromartos <geromartos1907@gmail.com>
2024-05-02 11:26:56 -0300

    ARREGLE TODO LO DE EITAN
    EITU PUTO DE MIERDA

--- clase18_4/lib/core/app_router.dart
+++ clase18_4/lib/core/app_router.dart
@@ -1,5 +1,5 @@
 import 'package:clase18_4/presentation/Screens/home_screen.dart';
-import 'package:clase18_4/presentation/Screens/login_screen.dart';
+import 'package:clase18_4/presentation/Screens/Login_screen.dart';
 
 import 'package:go_router/go_router.dart';
 
@@ -9,14 +9,12 @@ initialLocation: '/login',
   
  routes: [
 
-  GoRoute(   path:'/login',
-  name: LoginScreen.name, 
-  builder: (context, state) => LoginScreen() ,
-  ),
+  GoRoute(path:'/login', 
+  builder: (context, state) => LoginScreen() ,),
 
-  GoRoute(path:'/home', 
+  GoRoute(path:'/home',
   name: HomeScreen.name,
-  builder: (context, state) =>  HomeScreen(),
+  builder: (context, state) =>  HomeScreen(userName: state.extra as String),
   ),
 
 
--- clase18_4/lib/presentation/Screens/home_screen.dart
+++ clase18_4/lib/presentation/Screens/home_screen.dart
@@ -1,20 +1,29 @@
+import 'package:clase18_4/presentation/Screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-// ignore: must_be_immutable
-class HomeScreen extends StatelessWidget{
-  static const String name = 'home';
-  String usuario;
-  HomeScreen({super.key, this.usuario = ''});
+class HomeScreen extends StatelessWidget {
+static const String name = 'home';
+
+String userName;
+ HomeScreen({super.key , this.userName = ""});
+  
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: Center(
+
+
+    return  Scaffold(
+      appBar: AppBar (
+        title: const Text('Home')),
+      body:  Center(
+        child: Column (mainAxisAlignment: MainAxisAlignment.center,
+            
+            children: [
+              const Text('Bienvenido', style:  TextStyle( fontSize: 30),),
+               Text(userName, style: const TextStyle(color: Color.fromRGBO(58, 11, 189, 1) , fontSize: 50),),
         
-        child: Text('Welcome $usuario'),
+          ],),
       ),
     );
   }
